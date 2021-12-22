9dd1749ff39cf792aeb6121e83aca3aa7711987c
ailugarcia98 <ailu_tunci@hotmail.com>
2021-12-21 21:12:15 -0300

    build: add findWalletByUserId

--- src/service/wallet_service.js
+++ src/service/wallet_service.js
@@ -2,23 +2,29 @@
 const WalletRepository = require("../postgres/repository/wallet_repository");
 
 const findAll = async () =>
-  wrapWithUnknownError(() => WalletRepository.findAll(), "Unable to find all categories due to unknown error");
+  wrapWithUnknownError(() => WalletRepository.findAll(), "Unable to find all wallets due to unknown error");
 
-const findById = async idCategory =>
+const findById = async idWallet =>
   wrapWithUnknownError(
-    () => WalletRepository.findById(idCategory),
-    `Unable to find category ${idCategory} due to unknown error`,
+    () => WalletRepository.findById(idWallet),
+    `Unable to find wallet ${idWallet} due to unknown error`,
   );
 
-const create = async newCategoryDto =>
+const findByUserId = async idUser =>
   wrapWithUnknownError(
-    () => WalletRepository.create(newCategoryDto),
-    `Unable to create category ${newCategoryDto} due to unknown error`,
+    () => WalletRepository.findByUserId(idUser),
+    `Unable to find wallet to user ${idUser} due to unknown error`,
+  );
+
+const create = async newWallet =>
+  wrapWithUnknownError(
+    () => WalletRepository.create(newWallet),
+    `Unable to create wallet ${newWallet} due to unknown error`,
   );
 
 const wrapWithUnknownError = (process, message) =>
   process().catch(err => {
-    console.error("Unable to operate with category service due to error", err);
+    console.error("Unable to operate with wallet service due to error", err);
     return new Error({ kind: "UNKNOWN_ERROR", message });
   });
 
@@ -27,6 +33,7 @@ const countWallet = (process, message) =>
 
 module.exports = {
   findById,
+  findByUserId,
   findAll,
   create,
   countWallet,
